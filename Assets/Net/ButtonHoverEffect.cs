fabc86bfc40ffccda4ab6d444ea4050e82d66d88
Ricky-run <anzilirdyneev@gmail.com>
2026-07-09 21:00:41 +0900

    Кнопки отзывчивые

--- Assets/Net/ButtonHoverEffect.cs
+++ Assets/Net/ButtonHoverEffect.cs
@@ -4,22 +4,22 @@ using UnityEngine.EventSystems;
 public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private Vector3 originalScale;
-    [Tooltip("На сколько увеличится кнопка. 1.1 — это увеличение на 10%")]
+    [Tooltip("")]
     public float hoverScaleMultiplier = 1.1f;
 
     void Start()
     {
-        // Запоминаем начальный размер кнопки при старте игры
+      
         originalScale = transform.localScale;
     }
 
-    // Метод срабатывает автоматически, когда курсор мыши заходит на кнопку
+   
     public void OnPointerEnter(PointerEventData eventData)
     {
         transform.localScale = originalScale * hoverScaleMultiplier;
     }
 
-    // Метод срабатывает автоматически, когда курсор мыши уходит с кнопки
+   
     public void OnPointerExit(PointerEventData eventData)
     {
         transform.localScale = originalScale;
